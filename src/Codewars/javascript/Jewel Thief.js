3a01f04f69b9ac265314b4c445c2838d0cab6634
sgerodes <sgerodes@gmail.com>
2018-07-22 19:39:06 +0200

    add Jewel Thief solution

---
+++ src/Codewars/javascript/Jewel Thief.js
@@ -0,0 +1,19 @@
+var crack = function(safe) {
+    code = ""
+    unlocking = "click"
+    for (let i = 0; i < 3; ++i){
+        for (let i = 0; i < 100; ++i){
+            let num = i < 10 ? "0" + i : i;
+            for (let direction of ["L", "R"]){
+                comb = direction + num;
+                if (safe.unlock(code + comb) == unlocking){
+                    code += comb;
+                }
+            }
+        }
+        unlocking += "-click"
+        code += "-"
+    }
+    safe.unlock(code.substring(0, 11))
+    return safe.open();
+}
