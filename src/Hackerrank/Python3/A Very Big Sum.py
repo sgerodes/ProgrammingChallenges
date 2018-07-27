3a3b3899ff0cc2fe47e67e0b12e480df81fa5990
sgerodes <sgerodes@gmail.com>
2018-07-27 11:13:32 +0200

    add several Hackerrank python solutions

---
+++ src/Hackerrank/Python3/A Very Big Sum.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+summ=0
+for zahl in arr:
+    summ += zahl
+
+print (summ)
