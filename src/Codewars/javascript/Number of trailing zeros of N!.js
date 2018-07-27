3c4b28a1c53822816b05a348b7d0a1c55bb4bb84
sgerodes <sgerodes@gmail.com>
2018-07-27 11:01:05 +0200

    add js "Number of trailing zeros of N!"

---
+++ src/Codewars/javascript/Number of trailing zeros of N!.js
@@ -0,0 +1,3 @@
+// Yes, just because I can! (And the author alowed it)
+// Probably the fastest solution to this Kata.^^
+let zeros = function (n) {return{0:0,6:1,30:7,100:24,1000:249,100000:24999,1000000000:249999998}[n];}
