d27fe789eb20058011f73f33b303ab0e8c73cd34
sgerodes <sgerodes@gmail.com>
2018-07-27 11:14:04 +0200

    add py Diagonal Difference

---
+++ src/Hackerrank/Python3/Diagonal Difference.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+a = []
+for a_i in range(n):
+    a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+    a.append(a_t)
+
+primedig=0
+secdig=0
+for i in range(n):
+    primedig += a[i][i]
+
+for i in range(n):
+    secdig += a[i][n-i-1]
+
+print (abs(primedig-secdig))
