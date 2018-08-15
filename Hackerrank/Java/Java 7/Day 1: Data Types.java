b779adb7d69a667803bcf67fda51259c67f7e12b
sgerodes <sgerodes@gmail.com>
2018-08-15 13:33:43 +0200

    add Hackerrank java Day 1: Data Types

---
+++ Hackerrank/Java/Java 7/Day 1: Data Types.java
@@ -0,0 +1,16 @@
+        /* Declare second integer, double, and String variables. */
+        int i2=scan.nextInt();
+                double d2=scan.nextDouble();
+                String s2=scan.nextLine();
+                s2=scan.nextLine();
+                /* Read and save an integer, double, and String to your variables.*/
+                // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+                /* Print the sum of both integer variables on a new line. */
+                System.out.println(i+i2);
+                /* Print the sum of the double variables on a new line. */
+                System.out.println(d+d2);
+
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+                System.out.println(s+s2);
