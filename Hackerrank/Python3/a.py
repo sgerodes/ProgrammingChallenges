e34feacba727aebb5017b641de7ee44afa43fdec
sgerodes <sgerodes@gmail.com>
2018-09-13 15:35:12 +0300

    add Hackerrank py Birthday Cake Candles

---
+++ Hackerrank/Python3/a.py
@@ -0,0 +1,19 @@
+import os
+from collections import Counter
+
+def birthdayCakeCandles(ar):
+    return Counter(ar)[max(ar)]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
