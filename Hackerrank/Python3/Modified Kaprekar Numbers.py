af16cab225286e0681f8f61d78229267682a26a3
sgerodes <sgerodes@gmail.com>
2018-09-16 22:56:35 +0200

    add Hackerrank py Modified Kaprekar Numbers

---
+++ Hackerrank/Python3/Modified Kaprekar Numbers.py
@@ -0,0 +1,18 @@
+def kaprekarNumbers(p, q):
+    return " ".join([str(i) for i in range(p, q + 1) if is_kaprekar(i)]) or "INVALID RANGE"
+
+
+def is_kaprekar(num):
+    s = str(num ** 2)
+    l = len(s) // 2
+    return int(s[:l] if l > 0 else 0) + int(s[l:]) == num
+
+
+if __name__ == '__main__':
+    p = int(input())
+
+    q = int(input())
+
+    answer = kaprekarNumbers(p, q)
+
+    print(answer)
